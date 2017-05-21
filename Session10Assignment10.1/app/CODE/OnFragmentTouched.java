0dc5c2da9fa38ab77b55ee42ceb849877b4b344a
Neelam-Venaktesh <n.venkateshvvv@gmail.com>
2017-05-21 17:16:16 +0530

    Session10Assignment10.1
    Android Session10 Assignment10.1

---
+++ Session10Assignment10.1/app/CODE/OnFragmentTouched.java
@@ -0,0 +1,10 @@
+package com.fernandofgallego.circularrevealfragment.sample;
+
+import android.app.Fragment;
+
+/**
+ * Created by gajendra kumar
+ */
+public interface OnFragmentTouched {
+    public void onFragmentTouched(Fragment fragment, float x, float y);
+}
